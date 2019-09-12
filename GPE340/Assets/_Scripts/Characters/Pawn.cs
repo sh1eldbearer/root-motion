25fb709b5c86db3aac899a31c02f9834809af9e7
Nathan Scott <natscott@uat.edu>
2019-09-11 20:18:48 -0700

    Updated animation root motion transforms/rotations/offsets and set up basic character movement

---
+++ GPE340/Assets/_Scripts/Characters/Pawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (anim == null)
+        {
+            anim = GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Pass the values on the "controller" from the input manager into the animator
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal") );
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+    }
+}
