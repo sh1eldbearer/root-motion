66f97001a4d0b500f1cfd59d83ee977cb97fac21
Nathan Scott <natscott@uat.edu>
2019-09-17 16:27:00 -0700

    Camera controller working, character rotation working (need to decide on slow camera follow and slow player rotation)

---
+++ GPE340/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float followSpeed = 3.5f;
+    public Vector3 initalOffset;
+
+    private Transform thisTf;
+    private Transform followTf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTf = this.transform;
+        followTf = followTarget.transform;
+        initalOffset = thisTf.position - followTf.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followTarget != null)
+        {
+            thisTf.position = Vector3.MoveTowards(thisTf.position, followTf.position + initalOffset, followSpeed * Time.deltaTime);
+        }
+    }
+}
--- GPE340/Assets/_Scripts/Characters/Pawn.cs
+++ GPE340/Assets/_Scripts/Characters/Pawn.cs
@@ -4,20 +4,64 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
-    [HideInInspector] public Animator anim; // The animator that controls this character
+    /* Public Properties */
+    public float moveSpeed = 3.5f;
+    public float turnSpeed = 60.0f;
+    public Camera myCamera;
+
+    /* Private Properties */
+    [Header("Component References (Can be set in inspector, but will be auto-assigned in code if left empty.")]
+    [SerializeField, Tooltip("This object's Transform component")] private Transform tf;
+    [SerializeField] private Animator anim; // The animator that controls this character
 
     // Start is called before the first frame update
     void Start()
     {
         /* Component reference assignemnts */
-        anim = GetComponent<Animator>();
+        tf = this.GetComponent<Transform>();
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get the world vector that we want to move
+        Vector3 worldMoveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // Normalize it to allow controllers and keyboards to function the same
+        worldMoveVector.Normalize();
+
+        // Find local version of the worldMoveVector (relative to the object's transform)
+        Vector3 localMoveVector = transform.InverseTransformDirection(worldMoveVector);
+
         // Pass values from the input controller into the animator to generate movement
-        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Horizontal", localMoveVector.x * moveSpeed);
+        anim.SetFloat("Vertical", localMoveVector.z * moveSpeed);
+
+        HandleRotation();
+    }
+
+    void HandleRotation()
+    {
+        // Create a plane object for the plane the player is standing on
+        Plane groundPlane = new Plane(Vector3.up, tf.position);
+
+        // Create a ray from camera through the mouse position in the direction the camera is facing
+        Ray mouseRay = myCamera.ScreenPointToRay(Input.mousePosition);
+
+        // Raycast
+        float intersectDistance;
+        if (groundPlane.Raycast(mouseRay, out intersectDistance))
+        {
+            Vector3 collisionPoint = mouseRay.GetPoint(intersectDistance);
+
+            // Get rotation needed to look at that point
+            Quaternion targetRotation = Quaternion.LookRotation(collisionPoint - tf.position, tf.up);
+            tf.rotation = Quaternion.RotateTowards(tf.rotation, targetRotation, turnSpeed * Time.deltaTime) ;
+            //tf.LookAt(collisionPoint);
+        }
+        else
+        {
+            Debug.LogError("Camera is not looking at plane");
+        }
     }
 }
