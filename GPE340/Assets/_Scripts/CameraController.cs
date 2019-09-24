2e5a7810d85a74340cf7b2f99102769ddb9b051d
Nathan Scott <natscott@uat.edu>
2019-09-23 23:17:24 -0700

    Camera follow matches follow target speed; no longer uses coroutine

--- GPE340/Assets/_Scripts/CameraController.cs
+++ GPE340/Assets/_Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,33 +11,40 @@ public class CameraController : MonoBehaviour
 
     private Transform thisTf;
     private Transform followTf;
+    private Pawn followPawn;
 
     // Start is called before the first frame update
     void Start()
     {
+        /* Component reference assignments */
         thisTf = this.transform;
         followTf = followTarget.transform;
-        initalOffset = thisTf.position - followTf.position;
-        StartCoroutine(UpdateCameraPosition());
+        followPawn = followTarget.GetComponent<Pawn>();
+
+        if (followTarget != null)
+        {
+            // Stores the camera's original position relative to its target
+            initalOffset = thisTf.position - followTf.position;
+            // Sets the camera's follow speed to match the move speed of its follow target
+            followSpeed = followPawn.moveSpeed;
+        }
+        else
+        {
+            // If I forgot to set a follow target for the camera, yells at me (but only in the editor)
+#if UNITY_EDITOR
+            Debug.LogError(string.Format("You forgot to set a follow target for {0}!", this.name));
+#endif
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-    }
-
-    private IEnumerator UpdateCameraPosition()
-    {
-        while (GameManager.gm.gameIsRunning)
+        if (GameManager.gm.gameIsRunning && followTarget != null)
         {
             // As long as the camera has a target to follow, updates the camera's position 
-            if (followTarget != null)
-            {
-                thisTf.position = Vector3.MoveTowards(thisTf.position, followTf.position + initalOffset,
-                    followSpeed * Time.deltaTime);
-            }
-
-            yield return null;
+            thisTf.position = Vector3.MoveTowards(thisTf.position, followTf.position + initalOffset,
+                followSpeed * Time.deltaTime);
         }
     }
 }
