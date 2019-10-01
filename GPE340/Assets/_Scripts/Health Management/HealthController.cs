7384942e48385f329ac7630d002e11388525ce7d
Nathan Scott <natscott@uat.edu>
2019-10-01 16:22:34 -0700

    HealthController, events, ui updating

---
+++ GPE340/Assets/_Scripts/Health Management/HealthController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class HealthController : MonoBehaviour
+{
+    [Header("Health Values")] public float currentHealth;
+
+    [SerializeField] private float maxHealth;
+
+    // Helper functions
+    public float healthPercent
+    {
+        get { return (currentHealth / maxHealth) * 100; }
+        set { currentHealth = maxHealth * value; }
+    }
+
+    [Header("UnityEvents")]
+    public UnityEvent OnDeathEvent;
+    public UnityEvent OnChangeHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealToFull();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damageTaken, Pawn source = null)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damageTaken, 0, maxHealth);
+        OnChangeHealth.Invoke();
+
+        if (source != null)
+        {
+            Debug.Log("Shot by: " + source.name);
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float healingTaken)
+    {
+        healthPercent = Mathf.Clamp(currentHealth + healingTaken, 0, maxHealth);
+        OnChangeHealth.Invoke();
+    }
+
+    private void Die()
+    {
+        // Whatever needs to be done when you die
+        SendMessage("OnDie");
+    }
+
+    public void KillMe()
+    {
+        currentHealth = 0;
+        OnChangeHealth.Invoke();
+        Die();
+    }
+
+    public void HealToFull()
+    {
+        currentHealth = maxHealth;
+        OnChangeHealth.Invoke();
+    }
+}
---
+++ GPE340/Assets/_Scripts/UIController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [Header("UI elements")]
+    public Text healthText;
+
+    [Header("Data Sources")]
+    public HealthController playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthUI()
+    {
+        if (playerHealth != null)
+        {
+            healthText.text = "Health: " + playerHealth.healthPercent + "%";
+        }
+        else
+        {
+            healthText.text = string.Empty;
+        }
+    }
+}
