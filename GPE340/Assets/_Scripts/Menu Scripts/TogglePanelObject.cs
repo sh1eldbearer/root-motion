47de6ec4412566a050e95efafb9aed7297167490
Nathan Scott <natscott@uat.edu>
2020-02-07 17:32:44 -0700

    Menu buttons now enable and disable appropriate panels

---
+++ GPE340/Assets/_Scripts/Menu Scripts/TogglePanelObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePanelObject : MonoBehaviour
+{
+    [Tooltip("The menu panel this button should enable."), 
+        SerializeField] private GameObject panelToEnable;
+    [Tooltip("The menu panel this button should disable."), 
+        SerializeField] private GameObject panelToDisable;
+
+    /// <summary>
+    /// Toggles the panel as visible or invisible
+    /// </summary>
+    public void TogglePanel()
+    {
+        panelToEnable.SetActive(true);
+        panelToDisable.SetActive(false);
+    }
+}
