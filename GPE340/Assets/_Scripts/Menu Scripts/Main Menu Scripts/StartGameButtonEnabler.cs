1bbf77ab962e7f4ec1de25659359ccb1483a42f0
Nathan Scott <natscott@uat.edu>
2020-03-03 15:41:32 -0700

    Basic start game button enabling/disabling when all active players have selected their colors

---
+++ GPE340/Assets/_Scripts/Menu Scripts/Main Menu Scripts/StartGameButtonEnabler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGameButtonEnabler : MonoBehaviour
+{
+    #region Private Properties
+#pragma warning disable CS0649
+    [Tooltip("The Button component for this button."),
+        SerializeField] private Button _button;
+#pragma warning restore CS0649
+    #endregion
+
+    #region Public Properties
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MonitorReadyStatus());
+    }
+
+    private IEnumerator MonitorReadyStatus()
+    {
+        while (true)
+        {
+            if ((!MainMenuManager.mainMenuMgr.P1ObjectGroup.isActiveAndEnabled || MainMenuManager.mainMenuMgr.P1ObjectGroup.SelectedIndex > 0) &&
+                (!MainMenuManager.mainMenuMgr.P2ObjectGroup.isActiveAndEnabled || MainMenuManager.mainMenuMgr.P2ObjectGroup.SelectedIndex > 0) &&
+                (!MainMenuManager.mainMenuMgr.P3ObjectGroup.isActiveAndEnabled || MainMenuManager.mainMenuMgr.P3ObjectGroup.SelectedIndex > 0) &&
+                (!MainMenuManager.mainMenuMgr.P4ObjectGroup.isActiveAndEnabled || MainMenuManager.mainMenuMgr.P4ObjectGroup.SelectedIndex > 0))
+            {
+                _button.interactable = true;
+            }
+            else
+            {
+                _button.interactable = false;
+            }
+
+            yield return null;
+        }
+    }
+}
