a1fbcbb329549840d7dead58182f7f685ffd3e30
Nathan Scott <natscott@uat.edu>
2020-03-06 06:13:19 -0700

    GameMenuManager created and game scene lighting changes

---
+++ GPE340/Assets/_Scripts/Menu Scripts/Game Menu Scripts/GameMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuManager : MonoBehaviour
+{
+    #region Private Properties
+#pragma warning disable CS0649
+
+#pragma warning restore CS0649
+    #endregion
+
+    #region Public Properties
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
