ccc780eb6b905fb4dc743b04930f4bde8a74f734
Nathan Scott <natscott@uat.edu>
2020-04-30 06:27:29 -0700

    Removed raycast, checks, and damage functionality into WeaponBehavior base class (since all weapons will use this)

--- GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/PistolBehavior.cs
+++ GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/PistolBehavior.cs
@@ -12,31 +12,7 @@ public class PistolBehavior : WeaponBehavior
         // If the weapon is on cooldown, it won't fire
         if (_fireCooldownTimer <= 0f)
         {
-            // Plays the weapon's muzzle flash particle effect
-            _weaponModelData.PlayMuzzleFlash();
-
-            // TODO: Use line renderer to generate a visual effect showing the path of the shot
-
-            // Looks to see if the shot hits anything
-            RaycastHit hitInfo;
-            if (Physics.Raycast(_weaponModelData.RaycastOriginTransform.position,
-                _weaponModelData.RaycastOriginTransform.forward, out hitInfo,
-                _pawnData.InventoryMgr.EquippedWeaponRange))
-            {
-                // Checks to see if the object can take damage
-                IDamageable damageable = hitInfo.collider.GetComponentInChildren<IDamageable>();
-
-                if (damageable != null)
-                {
-                    // TODO: Remove debug.log later
-                    Debug.Log(
-                        $"{_pawnData.gameObject.name} shot {hitInfo.collider.name} with a {WeaponType.Pistol.ToString()} " +
-                        $"for {_pawnData.InventoryMgr.EquippedWeaponDamage} damage.");
-
-                    // Has the other object take damage
-                    damageable.TakeDamage(_pawnData.InventoryMgr.EquippedWeaponDamage);
-                }
-            }
+            base.Shoot();
 
             // Starts the weapon's cooldown timer
             StartCoroutine(CooldownTimer());
--- GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/WeaponBehavior.cs
+++ GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/WeaponBehavior.cs
@@ -46,9 +46,36 @@ public abstract class WeaponBehavior : MonoBehaviour, IShootable
 
     }
 
+    /// <summary>
+    /// Fires this weapon.
+    /// </summary>
     public virtual void Shoot()
     {
+        // Plays the weapon's muzzle flash particle effect
+        _weaponModelData.PlayMuzzleFlash();
 
+        // TODO: Use line renderer to generate a visual effect showing the path of the shot
+
+        // Looks to see if the shot hits anything
+        RaycastHit hitInfo;
+        if (Physics.Raycast(_weaponModelData.RaycastOriginTransform.position,
+            _weaponModelData.RaycastOriginTransform.forward, out hitInfo,
+            _pawnData.InventoryMgr.EquippedWeaponRange))
+        {
+            // Checks to see if the object can take damage
+            IDamageable damageable = hitInfo.collider.GetComponentInChildren<IDamageable>();
+
+            if (damageable != null)
+            {
+                // TODO: Remove debug.log later
+                Debug.Log(
+                    $"{_pawnData.gameObject.name} shot {hitInfo.collider.name} with a {WeaponType.Pistol.ToString()} " +
+                    $"for {_pawnData.InventoryMgr.EquippedWeaponDamage} damage.");
+
+                // Has the other object take damage
+                damageable.TakeDamage(_pawnData.InventoryMgr.EquippedWeaponDamage);
+            }
+        }
     }
 
     /// <summary>
