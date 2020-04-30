d23c74741959835434d29dec537d80edde9a7538
Nathan Scott <natscott@uat.edu>
2020-04-30 03:25:52 -0700

    Changed LockCanvasRotation class name to better reflect purpose

--- GPE340/Assets/_Scripts/UI/LockPawnCanvasRotation.cs
+++ GPE340/Assets/_Scripts/UI/LockPawnCanvasRotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LockCanvasRotation : StateMachineBehaviour
+public class LockPawnCanvasRotation : StateMachineBehaviour
 {
     #region Private Properties
     [Tooltip("This agent's data component."),
