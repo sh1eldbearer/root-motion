28c1a2f11ae47e54388f688a7d040f502969bded
Nathan Scott <natscott@uat.edu>
2020-04-30 06:30:06 -0700

    Added other weapon behavior scripts and attached to player prefab

---
+++ GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/SniperBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperBehavior : MonoBehaviour
+{
+    #region Private Properties
+#pragma warning disable CS0649
+
+#pragma warning restore CS0649
+    #endregion
+
+    #region Public Properties
+
+    #endregion
+	
+	// Awake is called before Start
+	private void Awake()
+	{
+		
+	}
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
