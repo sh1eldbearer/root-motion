bb999be901c777c0c4df83c3ac59f26434879600
Nathan Scott <natscott@uat.edu>
2020-04-30 06:43:19 -0700

    Condensed add and remove listener functions on PauseManager

--- GPE340/Assets/_Scripts/PauseManager.cs
+++ GPE340/Assets/_Scripts/PauseManager.cs
@@ -31,106 +31,70 @@ public class PauseManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Adds a listener to the OnPause event.
-    /// </summary>
-    /// <param name="call">The name of the function to call when OnPause is invoked.</param>
-    public void AddOnPauseListener(UnityAction call)
-    {
-        // Adding a persistent listener allows me to visualize when events have been added
-        #if UNITY_EDITOR
-            UnityEventTools.AddPersistentListener(_onPause, call);
-        #else
-            _onPause.AddListener(call);
-        #endif
-    }
-
-    /// <summary>
     /// Adds multiple listeners to the OnPause event.
     /// </summary>
     /// <param name="calls">The names of the functions to call when OnPause is invoked.</param>
-    public void AddOnPauseListeners(params UnityAction[] calls)
+    public void AddOnPauseListener(params UnityAction[] calls)
     {
         foreach (UnityAction call in calls)
         {
-            AddOnPauseListener(call);
+            // Adding a persistent listener allows me to visualize when events have been added
+            #if UNITY_EDITOR
+                UnityEventTools.AddPersistentListener(_onPause, call);
+            #else
+                _onPause.AddListener(call);
+            #endif
         }
-    }
-
-    /// <summary>
-    /// Removes a listener from the OnPause event.
-    /// </summary>
-    /// <param name="call">The name of the function to remove from the OnPause invoke array.</param>
-    public void RemoveOnPauseListener(UnityAction call)
-    {
-        // Removing a persistent listener allows me to visualize when events have been removed
-        #if UNITY_EDITOR
-            UnityEventTools.RemovePersistentListener(_onPause, call);
-        #else
-            _onUnpause.AddListener(call);
-        #endif
     }
 
     /// <summary>
     /// Removes multiple listeners from the OnPause event.
     /// </summary>
     /// <param name="calls">The names of the functions to remove from the OnPause invoke array.</param>
-    public void RemoveOnPauseListeners(params UnityAction[] calls)
+    public void RemoveOnPauseListener(params UnityAction[] calls)
     {
         foreach (UnityAction call in calls)
-        {
-            RemoveOnPauseListener(call);
+            {
+            // Removing a persistent listener allows me to visualize when events have been removed
+            #if UNITY_EDITOR
+                UnityEventTools.RemovePersistentListener(_onPause, call);
+            #else
+                _onUnpause.AddListener(call);
+            #endif
         }
-    }
-
-    /// <summary>
-    /// Adds a listener to the OnUnpause event.
-    /// </summary>
-    /// <param name="call">The name of the function to call when OnUnpause is invoked.</param>
-    public void AddOnUnpauseListener(UnityAction call)
-    {
-        // Adding a persistent listener allows me to visualize when events have been added
-        #if UNITY_EDITOR
-            UnityEventTools.AddPersistentListener(_onUnpause, call);
-        #else
-            _onUnpause.AddListener(call);
-        #endif
     }
 
     /// <summary>
     /// Adds multiple listeners to the OnUnpause event.
     /// </summary>
     /// <param name="calls">The names of the functions to call when OnUnpause is invoked.</param>
-    public void AddOnUnpauseListeners(params UnityAction[] calls)
+    public void AddOnUnpauseListener(params UnityAction[] calls)
     {
         foreach (UnityAction call in calls)
         {
-            AddOnUnpauseListener(call);
+            // Adding a persistent listener allows me to visualize when events have been added
+            #if UNITY_EDITOR
+                UnityEventTools.AddPersistentListener(_onUnpause, call);
+            #else
+                _onUnpause.AddListener(call);
+            #endif
         }
-    }
-
-    /// <summary>
-    /// Removes a listener from the OnUnpause event.
-    /// </summary>
-    /// <param name="call">The name of the function to remove from the OnUnpause invoke array.</param>
-    public void RemoveOnUnpauseListener(UnityAction call)
-    {
-        // Removing a persistent listener allows me to visualize when events have been removed
-        #if UNITY_EDITOR
-            UnityEventTools.AddPersistentListener(_onUnpause, call);
-        #else
-            _onUnpause.RemoveListener(call);
-        #endif
     }
 
     /// <summary>
     /// Removes multiple listeners from the OnUnpause event.
     /// </summary>
     /// <param name="calls">The names of the functions to remove from the OnUnpause invoke array.</param>
-    public void RemoveOnUnpauseListeners(params UnityAction[] calls)
+    public void RemoveOnUnpauseListener(params UnityAction[] calls)
     {
         foreach (UnityAction call in calls)
         {
-            RemoveOnUnpauseListener(call);
+            // Removing a persistent listener allows me to visualize when events have been removed
+            #if UNITY_EDITOR
+                UnityEventTools.AddPersistentListener(_onUnpause, call);
+            #else
+                _onUnpause.RemoveListener(call);
+            #endif
         }
     }
 
