20bb81b0960fea6d4ad2af0ae4c2d4e7ef7dea51
Nathan Scott <natscott@uat.edu>
2020-04-30 06:23:07 -0700

    Changed Pistol Behavior to inherit from weapon behavior; removed unused interface

--- GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/PistolBehavior.cs
+++ GPE340/Assets/_Scripts/Weapons/Weapon Behaviors/PistolBehavior.cs
@@ -1,63 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using Utility.Enums;
 
-public class PistolBehavior : MonoBehaviour, IWeapon, IShootable
-{
-    // TODO: commenting pass
-    #region Private Properties
-#pragma warning disable CS0649
-    [Tooltip("The PawnData component for this pawn."),
-        SerializeField] private PawnData _pawnData;
-    private WeaponModelData _weaponModelData;
-#pragma warning restore CS0649
-    #endregion
-
-    #region Public Properties
-
-    #endregion
-	
-	// Awake is called before Start
-	private void Awake()
-	{
-		// Component reference assignments
-        if (_pawnData == null)
-        {
-            _pawnData = this.gameObject.GetComponentInParent<PawnData>();
-        }
-        if (_weaponModelData == null)
-        {
-            _weaponModelData = this.gameObject.GetComponent<WeaponModelData>();
-        }
-	}
-
-    // Start is called before the first frame update
-    private void Start()
+public class PistolBehavior : WeaponBehavior
+{ 
+    /// <summary>
+    /// Fires this weapon.
+    /// </summary>
+    public override void Shoot()
     {
-        
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
-    }
-
-    public void Shoot()
-    {
-        int weaponIndex = _pawnData.InventoryMgr.FindWeaponSlotByType(WeaponType.Pistol);
-
-        RaycastHit hitInfo;
-        if (Physics.Raycast(_weaponModelData.RaycastOriginTransform.position,
-            _weaponModelData.RaycastOriginTransform.forward, out hitInfo, _pawnData.InventoryMgr.EquippedWeaponRange))
+        // If the weapon is on cooldown, it won't fire
+        if (_fireCooldownTimer <= 0f)
         {
-            IDamageable damageable = hitInfo.collider.GetComponentInChildren<IDamageable>();
+            // Plays the weapon's muzzle flash particle effect
+            _weaponModelData.PlayMuzzleFlash();
 
-            if (damageable != null)
+            // TODO: Use line renderer to generate a visual effect showing the path of the shot
+
+            // Looks to see if the shot hits anything
+            RaycastHit hitInfo;
+            if (Physics.Raycast(_weaponModelData.RaycastOriginTransform.position,
+                _weaponModelData.RaycastOriginTransform.forward, out hitInfo,
+                _pawnData.InventoryMgr.EquippedWeaponRange))
             {
-                Debug.Log($"{_pawnData.gameObject.name} shot {hitInfo.collider.name} with a {WeaponType.Pistol.ToString()} " +
-                          $"for {_pawnData.InventoryMgr.EquippedWeaponDamage} damage.");
-                damageable.TakeDamage(_pawnData.InventoryMgr.EquippedWeaponDamage);
+                // Checks to see if the object can take damage
+                IDamageable damageable = hitInfo.collider.GetComponentInChildren<IDamageable>();
+
+                if (damageable != null)
+                {
+                    // TODO: Remove debug.log later
+                    Debug.Log(
+                        $"{_pawnData.gameObject.name} shot {hitInfo.collider.name} with a {WeaponType.Pistol.ToString()} " +
+                        $"for {_pawnData.InventoryMgr.EquippedWeaponDamage} damage.");
+
+                    // Has the other object take damage
+                    damageable.TakeDamage(_pawnData.InventoryMgr.EquippedWeaponDamage);
+                }
             }
+
+            // Starts the weapon's cooldown timer
+            StartCoroutine(CooldownTimer());
         }
     }
 }
